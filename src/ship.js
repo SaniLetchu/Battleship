83766d50e8c338545ff2de96d68d78dd379f2574
Sani Letchu <sani.letchu@gmail.com>
2022-06-14 10:36:51 +0300

    Passed ship tests

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,31 @@
 const ship = (length) => {
-  const length = length;
+  const container = [];
+  let vertical = false;
+  for(let i = 0; i < length; i+=1) {
+    container.push(0);
+  }
+  const isVertical = () => vertical;
+  const rotate = () => {
+    if(vertical) {
+      vertical = false;
+    }
+    else {
+      vertical = true;
+    }
+  }
+  const hit = (position) => {
+    container[position] = 1;
+  }
+  const isSunk = () => {
+    for(let i = 0; i < container.length; i+=1) {
+      if(container[i] === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return {container, isSunk, isVertical, hit, rotate};
+
 };
+
+export default ship;
