43eea0efa918435d166eef5a4748504c8c63a93a
Sani Letchu <sani.letchu@gmail.com>
2022-06-14 21:36:52 +0300

    Created grids for boards and added background for both gameboards

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import { player, AI } from "./player";
 import gameboard from "./gameboard";
 import ship from "./ship";
+
+const playerBoard = document.querySelector(".playerboard");
+const enemyBoard = document.querySelector(".enemyboard");
+
+// Playerboard
+for(let i = 0; i < 10; i+=1) {
+  for(let j = 0; j < 10; j+=1) {
+    const div = document.createElement("div");
+    div.classList.add(`${j}${i}`);
+    playerBoard.appendChild(div);
+  }
+}
+
+// Enemyboard
+for(let i = 0; i < 10; i+=1) {
+  for(let j = 0; j < 10; j+=1) {
+    const div = document.createElement("div");
+    div.classList.add(`${j}${i}`);
+    enemyBoard.appendChild(div);
+  }
+}
